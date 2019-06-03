442b8b099c12c4cfb0b873c1cd7a789a1d82700b
LucasSouza6 <lucasosouza66@gmail.com>
2019-06-03 13:23:03 -0300

    Bake documentos model

---
+++ src/Model/Table/DocumentosTable.php
@@ -0,0 +1,63 @@
+<?php
+namespace App\Model\Table;
+
+use Cake\ORM\Query;
+use Cake\ORM\RulesChecker;
+use Cake\ORM\Table;
+use Cake\Validation\Validator;
+
+/**
+ * Documentos Model
+ *
+ * @method \App\Model\Entity\Documento get($primaryKey, $options = [])
+ * @method \App\Model\Entity\Documento newEntity($data = null, array $options = [])
+ * @method \App\Model\Entity\Documento[] newEntities(array $data, array $options = [])
+ * @method \App\Model\Entity\Documento|bool save(\Cake\Datasource\EntityInterface $entity, $options = [])
+ * @method \App\Model\Entity\Documento saveOrFail(\Cake\Datasource\EntityInterface $entity, $options = [])
+ * @method \App\Model\Entity\Documento patchEntity(\Cake\Datasource\EntityInterface $entity, array $data, array $options = [])
+ * @method \App\Model\Entity\Documento[] patchEntities($entities, array $data, array $options = [])
+ * @method \App\Model\Entity\Documento findOrCreate($search, callable $callback = null, $options = [])
+ */
+class DocumentosTable extends Table
+{
+    /**
+     * Initialize method
+     *
+     * @param array $config The configuration for the Table.
+     * @return void
+     */
+    public function initialize(array $config)
+    {
+        parent::initialize($config);
+
+        $this->setTable('documentos');
+        $this->setDisplayField('id');
+        $this->setPrimaryKey('id');
+    }
+
+    /**
+     * Default validation rules.
+     *
+     * @param \Cake\Validation\Validator $validator Validator instance.
+     * @return \Cake\Validation\Validator
+     */
+    public function validationDefault(Validator $validator)
+    {
+        $validator
+            ->integer('id')
+            ->allowEmptyString('id', 'create');
+
+        $validator
+            ->scalar('nome')
+            ->maxLength('nome', 255)
+            ->requirePresence('nome', 'create')
+            ->allowEmptyString('nome', false);
+
+        $validator
+            ->scalar('descricao')
+            ->requirePresence('descricao', 'create')
+            ->allowEmptyString('descricao', false);
+
+        return $validator;
+    }
+}
